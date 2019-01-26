0f2468c6a504e0bd787de50c1ae292b6e643340e
Princeton Evans <princetonevans@yahoo.com>
2019-01-25 22:57:21 -0600

    created authguard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,17 +4,21 @@ import { AdminComponent } from './admin/admin.component';
 import { ManageCustomersComponent } from './manage-customers/manage-customers.component';
 import { ManageContactsComponent } from './manage-contacts/manage-contacts.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const adminRoutes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
         children: [
           { path: 'customers', component: ManageCustomersComponent },
           { path: 'contacts', component: ManageContactsComponent },
+          { path: 'products', component: ManageProductsComponent },
           { path: '', component: AdminDashboardComponent }
         ]
       }
