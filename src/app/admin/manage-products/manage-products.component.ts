e93a389cf7001775ff34fb971a00d4b2a240de8b
Princeton Evans <princetonevans@yahoo.com>
2019-01-28 08:52:05 -0600

    set up data from firebase and template

--- src/app/admin/manage-products/manage-products.component.ts
+++ src/app/admin/manage-products/manage-products.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+interface Product {
+  name: string;
+  description: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-manage-products',
@@ -7,9 +16,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageProductsComponent implements OnInit {
 
-  constructor() { }
+  productCollection: AngularFirestoreCollection<Product>;
+  Products: Observable<Product[]>;
+  products: Observable<Product[]>;
+
+  constructor(public afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.productCollection = this.afs.collection('products');
+      this.products = this.productCollection.valueChanges();
   }
 
   cancel() {
