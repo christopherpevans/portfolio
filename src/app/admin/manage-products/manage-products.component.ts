70ed7fa77aa35267df6bc3d720e60db68a0c2055
Princeton Evans <princetonevans@yahoo.com>
2019-02-08 15:48:25 -0600

    can now add and delete products for firebase db

--- src/app/admin/manage-products/manage-products.component.ts
+++ src/app/admin/manage-products/manage-products.component.ts
@@ -1,57 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, fromDocRef } from 'angularfire2/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+  fromDocRef
+} from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import { MatSnackBar } from '@angular/material';
 import { Product } from 'src/app/models/product';
-
 
 @Component({
   selector: 'app-manage-products',
   templateUrl: './manage-products.component.html',
   styleUrls: ['./manage-products.component.scss']
 })
-export class ManageProductsComponent implements OnInit {
-
-  model = new Product('', '', '', '', null);
+export class ManageProductsComponent implements OnInit, OnChanges {
+  id: number;
+  model = new Product('', '', '', null);
   productCollection: AngularFirestoreCollection<Product>;
   Products: Observable<Product[]>;
   products: Observable<Product[]>;
-  types = ['Website Design', 'Social Media Ads', 'Print Design', 'Logo Design', 'Design Only (flyers, business cards, post cards, etc)',
-    'Printing', 'Package Deal (Multiple Services)'];
+  types = [
+    'Website Design',
+    'Social Media Ads',
+    'Print Design',
+    'Logo Design',
+    'Design Only (flyers, business cards, post cards, etc)',
+    'Printing',
+    'Package Deal (Multiple Services)'
+  ];
 
-  constructor(public afs: AngularFirestore,
-    public snackBar: MatSnackBar) { }
+  constructor(public afs: AngularFirestore, public snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.productCollection = this.afs.collection('products');
-    this.products = this.productCollection.valueChanges();
+    console.log((this.products = this.productCollection.valueChanges()));
   }
-
-  cancel() {
-
-  }
-
 
   onSubmit() {
-    const name: string = this.model.name;
-    const price: number = this.model.price;
-    const description: string = this.model.description;
-    const image: string = this.model.image;
-    console.log(this.model);
-    this.afs.collection('products').add({
-      name: name,
-      description: description,
-      image: image,
-      price: price
-    })
-      .then(function (docRef) {
-        console.log('Document successfully written! ' + docRef.id);
+    this.afs
+      .collection('products')
+      .doc(this.model.name)
+      .set(Object.assign({}, this.model));
+  }
+
+  remove(product: { name: string }) {
+    this.afs
+      .collection('products')
+      .doc(product.name)
+      .delete()
+      .then(function() {
+        console.log('Document successfully deleted!');
       })
-      .catch(function (error) {
-        console.error('Error writing document: ', error);
+      .catch(function(error) {
+        console.error('Error removing document: ', error);
       });
   }
 
+  ngOnChanges() {
+    console.log(this.products);
+  }
 }
